cd3b3a93870188ca65052ca00316342329317fff
Yao WANG <yao.wang@thoughtworks.com>
2023-03-14 16:59:49 +0800

    code init

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+
+inputs = []
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        input_ = request.form['input']
+        inputs.append(input_)
+        return str(len(inputs) - 1)
+
+
+@app.route('/<int:index_>')
+def get_input(index_):
+    try:
+        return inputs[int(index_)]
+    except Exception:
+        return ''
+
+
+if __name__ == '__main__':
+    app.run()
